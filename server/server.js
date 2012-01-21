61b83a2c2564ee9da15e6251e031628239541139
theDekel <thedekel@gmail.com>
2012-01-21 15:43:48 -0500

    some progress with the google api

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ var net = require("net"),
 
 var client = http.createClient(80, 'www.google.com');
 
+var imageurl = "";
+
 var headers ={
     'Host': "www.google.com",
     'Content-Type':'multipart/form-data'
@@ -10,16 +12,27 @@ var headers ={
 
 var searchstring = "http://www.chucksanimeshrine.com/animeblog/uploaded_images/Black_Rock_shooter_1-726069.jpg";
 
-var request = client.request("GET", "http://images.google.com/searchbyimage?image_url="+searchstring, headers);
+var imagerequest = client.request("GET", "http://images.google.com/searchbyimage?image_url="+searchstring, headers);
 
-request.on('response',function(res){
+imagerequest.on('response',function(res){
     res.on('data',function(chunk){
-        console.log(chunk.toString());
-    });
+//      console.log(chunk.toString());
+        data = /HREF=".+"/.exec(chunk.toString());
+        imageurl = data[0].substring(6,data[0].length-1);
+      console.log(imageurl)
     res.on('end',function(){
-        console.log('works');
     });
 });
 
-request.write("hello");
-request.end();
+var searchrequest = http.createClient(80,'www.google.com').request("GET", imageurl, {"Host":'www.google.com','Content-Type':"text/html"});
+searchrequest.on("response",function(res2){
+res2.on("data",function(chunk2){
+data2 = /<div class="gsfi".+dir="ltr".+<\/div>/.exec(chunk2.toString());
+});
+});
+searchrequest.write("yes");
+searchrequest.end();
+});
+
+imagerequest.write("hello");
+imagerequest.end();
