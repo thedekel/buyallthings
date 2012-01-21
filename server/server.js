92dbd4a14a34f00d62e719c714508945cc9e5116
theDekel <thedekel@gmail.com>
2012-01-21 13:27:02 -0500

    server gets response

--- server/server.js
+++ server/server.js
@@ -1,2 +1,25 @@
 var net = require("net"),
     http = require("http");
+
+var client = http.createClient(80, 'www.google.com');
+
+var headers ={
+    'Host': "www.google.com",
+    'Content-Type':'multipart/form-data'
+}
+
+var searchstring = "http://www.chucksanimeshrine.com/animeblog/uploaded_images/Black_Rock_shooter_1-726069.jpg";
+
+var request = client.request("GET", "http://images.google.com/searchbyimage?image_url="+searchstring, headers);
+
+request.on('response',function(res){
+    res.on('data',function(chunk){
+        console.log(chunk.toString());
+    });
+    res.on('end',function(){
+        console.log('works');
+    });
+});
+
+request.write("hello");
+request.end();
